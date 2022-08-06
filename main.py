79d615846739e0b4fe5a97951080f730f17b1b92
Andrey <andrey.a.pushkin@gmail.com>
2022-08-06 14:22:39 +0400

    add some stuff

--- main.py
+++ main.py
@@ -6,7 +6,6 @@
 First, a few callback functions are defined. Then, those functions are passed to
 the Application and registered at their respective places.
 Then, the bot is started and runs until we press Ctrl-C on the command line.
-
 Usage:
 Example of a bot-user conversation using ConversationHandler.
 Send /start to initiate the conversation.
@@ -29,14 +28,14 @@ if __version_info__ < (20, 0, 0, "alpha", 1):
         f"{TG_VER} version of this example, "
         f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
     )
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
 from telegram.ext import (
     Application,
     CommandHandler,
     ContextTypes,
     ConversationHandler,
     MessageHandler,
-    filters,
+    filters, CallbackContext, CallbackQueryHandler,
 )
 
 # Enable logging
@@ -45,7 +44,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-step1, step2, LOCATION, BIO = range(4)
+TYPEOF, WEIGHT, CRYPTO, CHECK = range(4)
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -53,90 +52,79 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     reply_keyboard = [["Sativa", "Indika", "Hash"]]
 
     await update.message.reply_text(
-        "Hi! My name is Professor Bot. I will hold a conversation with you. "
-        "Send /cancel to stop talking to me.\n\n"
-        "Are you a boy or a girl?",
+        "Choose what type of weed do u want?",
         reply_markup=ReplyKeyboardMarkup(
             reply_keyboard, one_time_keyboard=True, input_field_placeholder="Boy or Girl?"
         ),
     )
 
-    return step1
+    return TYPEOF
 
 
 async def typeof(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Stores the selected gender and asks for a photo."""
     user = update.message.from_user
-    logger.info("Gender of %s: %s", user.first_name, update.message.text)
-    await update.message.reply_text(
-        "I see! Please send me a photo of yourself, "
-        "so I know what you look like, or send /skip if you don't want to.",
-        reply_markup=ReplyKeyboardRemove(),
-    )
+    reply_keyboard = [["1", "2", "5"]]
 
-    return step2
+    await update.message.reply_text(
+        "How much grams do u want?",
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True
+        ),
+    )
+    return WEIGHT
 
 
 async def weight(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Stores the photo and asks for a location."""
+    text = update.message.text
+    if int(text) == 1:
+        pay_amount = 30
+    elif int(text) == 2:
+        pay_amount = 50
+    else:
+        pay_amount = 100
     user = update.message.from_user
-    reply_keyboard = [["1", "2", "5"]]
+    reply_keyboard = [["Bitcoin", "Ethereum"]]
 
     await update.message.reply_text(
-        "Hi! My name is Professor Bot. I will hold a conversation with you. "
-        "Send /cancel to stop talking to me.\n\n"
-        "Are you a boy or a girl?",
+        f"To pay {pay_amount}$. Choose the payment option",
         reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Boy or Girl?"
-        ),
+            reply_keyboard
+        )
     )
-
-    return LOCATION
-
-
-async def skip_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Skips the photo and asks for a location."""
-    user = update.message.from_user
-    logger.info("User %s did not send a photo.", user.first_name)
-    await update.message.reply_text(
-        "I bet you look great! Now, send me your location please, or send /skip."
-    )
-
-    return LOCATION
+    return CRYPTO
 
 
-async def location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def crypto(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Stores the location and asks for some info about the user."""
+    text = update.message.text
+    if text == "Bitcoin":
+        pay_amount = "Your Bitcoin address. "
+    else:
+        pay_amount = "Your Ethereum address "
     user = update.message.from_user
-    user_location = update.message.location
-    logger.info(
-        "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
-    )
+    keyboard = [
+        [
+            InlineKeyboardButton("Check status", callback_data="1")
+            ]
+        ]
+
     await update.message.reply_text(
-        "Maybe I can visit you sometime! At last, tell me something about yourself."
+        f"{pay_amount} ",
+        reply_markup=InlineKeyboardMarkup(keyboard)
     )
-
-    return BIO
-
-
-async def skip_location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Skips the location and asks for info about the user."""
-    user = update.message.from_user
-    logger.info("User %s did not send a location.", user.first_name)
-    await update.message.reply_text(
-        "You seem a bit paranoid! At last, tell me something about yourself."
-    )
-
-    return BIO
+    return CHECK
 
 
-async def bio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Stores the info about the user and ends the conversation."""
-    user = update.message.from_user
-    logger.info("Bio of %s: %s", user.first_name, update.message.text)
-    await update.message.reply_text("Thank you! I hope we can talk again some day.")
-
-    return ConversationHandler.END
+async def check_status(update: Update):
+    query = update.callback_query
+    query.answer()
+    if query.data == '1':
+        await update.message.reply_text(
+            "The payment is not done"
+        )
+        return CHECK
 
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -159,13 +147,12 @@ def main() -> None:
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
-            step1: [MessageHandler(filters.Regex("^(Sativa|Indika|Hash)$"), typeof)],
-            step2: [MessageHandler(filters.Regex("^(1|2|5)$"), weight), CommandHandler("skip", skip_photo)],
-            LOCATION: [
-                MessageHandler(filters.LOCATION, location),
-                CommandHandler("skip", skip_location),
+            TYPEOF: [MessageHandler(filters.Regex("^(Sativa|Indika|Hash)$"), typeof)],
+            WEIGHT: [MessageHandler(filters.Regex("^(1|2|5)$"), weight)],
+            CRYPTO: [
+                MessageHandler(filters.Regex("^(Bitcoin|Ethereum)$"), crypto),
             ],
-            BIO: [MessageHandler(filters.TEXT & ~filters.COMMAND, bio)],
+            CHECK: [MessageHandler(filters.Regex("^(1)$"), check_status)],
         },
         fallbacks=[CommandHandler("cancel", cancel)],
     )
